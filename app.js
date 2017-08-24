648b9867b64e76ed7897a1f9b7cced1e3c39081f
Danzonius <tycho.atsma@gmail.com>
2017-08-24 17:14:39 +0200

    Added main server file, including some socket functionality

---
+++ app.js
@@ -0,0 +1,53 @@
+// require express module
+var express = require('express');
+
+// init the application
+var app = express();
+
+// init a http server
+var http = require('http').Server(app);
+
+// require the socket.io module
+var io = require('socket.io')(http);
+
+// set view engine
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// use static files
+app.use('/public', express.static('./public'));
+
+// open websocket connection
+io.on('connection', function (socket) {
+
+    // small notification
+    console.log('A user connected with #id ' + socket.id);
+
+    // handle chat messages
+    socket.on('chat message', function (message) {
+
+        // send message to client
+        io.emit('chat message', socket.id + ' said: ' + message, { for: 'everyone' });
+    });
+
+    // handle typing
+    socket.on('user typing', function () {
+
+        // send message to client
+        io.emit('user typing', 'A user is typing...', { for: 'everyone' });
+    });
+});
+
+// small example route
+app.get('/', function (req, res) {
+
+    // render template
+    res.render('index');
+});
+
+// run the http server on port 3000
+http.listen(3000, function () {
+
+    // small notification
+    console.log('Running server on port: 3000');
+});
