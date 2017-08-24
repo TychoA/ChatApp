8434c8f0b77d86961d992cc81f5c99190060d73b
Danzonius <tycho.atsma@gmail.com>
2017-08-24 15:45:17 +0200

    Updated gruntfile and added jquery lib

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,40 +6,40 @@ require('load-grunt-tasks')(grunt);
 
 // set up configuration
 grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        concat: {
-                options: {
-                        sourceMap: true
-                },
-                dist: {
-                        src: './public/js/*.js',
-                        dest: 'public/src/concat.js'
-                }
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+        options: {
+            sourceMap: true
         },
-        babel: {
-                dist: {
-                        options: {
-                                sourceMap: true
-                        },
-                        src: "./public/src/concat.js" ,
-                        dest: "./public/src/es5.js"
-                }
-        },
-        sass: {
-                dist: {
-                        options: {
-                                style: "compact"
-                        },
-                        src:  './public/scss/main.scss',
-                        dest: './public/main.css'
-                }
-        },
-        uglify: {
-                build: {
-                        src: './public/src/es5.js',
-                        dest: './public/minified.min.js'
-                }
+        dist: {
+            src: './public/js/*.js',
+            dest: 'public/src/concat.js'
         }
+    },
+    babel: {
+        dist: {
+            options: {
+                sourceMap: true
+            },
+            src: "./public/src/concat.js" ,
+            dest: "./public/src/es5.js"
+        }
+    },
+    sass: {
+        dist: {
+            options: {
+                style: "compact"
+            },
+            src:  './public/scss/main.scss',
+            dest: './public/main.css'
+    }
+    },
+    uglify: {
+        build: {
+            src: './public/src/es5.js',
+            dest: './public/minified.min.js'
+        }
+    }
 });
 
 // register the default tasks
